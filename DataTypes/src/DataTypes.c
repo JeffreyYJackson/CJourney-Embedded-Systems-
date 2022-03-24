ce35da27bd70f464f84668078b9ca09776f4be5f
Jeffrey Jackson <jeffrey.y.jackson@gmail.com>
2022-03-24 20:09:08 +0300

    DataTypes

---
+++ DataTypes/src/DataTypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main(){
+
+	int num = 11; 
+	float gpa = 3.9; //For decimals
+	double humidity = 64.3; //For decimals. 2 times more precise than float
+	
+	char grade = 'A';
+	char name[] = "Jeffrey"; // Creates an array of characters. Alternative to "string" from other languages.
+
+
+}
--- HelloWorld/HelloWorld.c
+++ HelloWorld/HelloWorld.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World! %i\n");
+    printf("Hello World! \n");
 }

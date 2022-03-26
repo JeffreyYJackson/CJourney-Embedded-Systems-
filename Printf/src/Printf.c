e43da331caa9ac916e4f59ad5ce1451b5ae276c7
Jeffrey Jackson <jeffrey.y.jackson@gmail.com>
2022-03-27 02:00:39 +0300

    Basic printf

---
+++ Printf/src/Printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	int grade = 100;
+	char subject[] = "math";
+	printf("I got a %d in my %s exam\n", grade, subject);
+
+
+
+	/*
+		You can use format specifiers to format you strings inside of your "printf" function.
+		More than one specifiers can be used in a printf function. The order of the paramaters should be the same as the specifiers.
+
+
+		%d > integers  ("%i" can be used instead of "%d" but it is not advised)
+		%s > string  
+
+
+	*/
+
+
+}

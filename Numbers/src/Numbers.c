10a7ee279a69b2f3403d71cfb1eeaf48a47befe9
Jeffrey Jackson <jeffrey.y.jackson@gmail.com>
2022-03-27 02:43:50 +0300

    Basic math functions

---
+++ Numbers/src/Numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+
+	printf("%f\n", pow(2, 3));
+	printf("%f\n", sqrt(36));
+	printf("%f\n", ceil(2.435));
+	printf("%f\n", floor(3.45678));
+
+}

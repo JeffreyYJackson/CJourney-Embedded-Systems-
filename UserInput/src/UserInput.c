c95bca0714067c49447b7f8246555a0e4ef95501
Jeffrey Jackson <jeffrey.y.jackson@gmail.com>
2022-03-28 00:18:56 +0300

    User input

---
+++ UserInput/src/UserInput.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main(){
+	/*
+	int age;
+	
+	printf("Enter your age: ");
+	scanf("%d", &age);
+	printf("%d! You look very young!\n", age);
+	
+	// "scanf function gets input from the user. &age in this example is a pointer to the age variable."
+
+	*/
+	/*
+	double gpa;
+	printf("Please enter your gpa: ");
+	scanf("%lf", &gpa);
+	printf("%f is bad for a smart student like you!\n", gpa);
+	*/
+
+	/*
+	char grade;
+	printf("Please enter your gpa: ");
+	scanf("%c", &grade);
+	printf("Your grade is %c\n", grade);
+	*/
+	/*
+	char name[20];
+	printf("What is your name? ");
+	scanf("%s", name);
+	printf("Nice to meet you %s\n", name);
+	*/
+	/*
+	char sentence[50];
+	printf("Enter your favourite sentence: ");
+	fgets(sentence, 50, stdin);
+	printf("%s is a nice sentence! \n", sentence);
+	*/
+	/*
+	int favNums[3];
+	printf("what are your favourite numbers? ");
+	scanf("%d%d%d", &favNums[0], &favNums[1], &favNums[2]);
+	printf("%d %d %d are great numbers!", favNums[0], favNums[1], favNums[2]);
+	*/
+
+
+
+
+}
